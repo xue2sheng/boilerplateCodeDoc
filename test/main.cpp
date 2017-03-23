593736d123085abfe7611b0703b73c122a0306c2
Andres Sevillano <andres.sevillano.vallejo@gmail.com>
2017-03-23 20:04:31 +0000

    Relative path

--- test/main.cpp
+++ test/main.cpp
@@ -142,12 +142,18 @@ BOOST_AUTO_TEST_CASE( test002 ) {
 
    // taken for granted that CMake copied default json schema file in the very directory where this test binary is generated
    std::string filename{"schema.json"};
+   std::string binary{boost::unit_test::framework::master_test_suite().argv[0]};
+   size_t found = binary.find_last_of("/\\");
+   if( found > 0 ) {
+        BOOST_TEST_MESSAGE( "found=" << found);
+        filename = binary.substr(0,found+1) + filename;
+   }
 
    int argc =boost::unit_test::framework::master_test_suite().argc;
    if( argc > 1) {
 	filename = std::string(boost::unit_test::framework::master_test_suite().argv[1]);
    }
-   BOOST_TEST_MESSAGE( "current binary=" << boost::unit_test::framework::master_test_suite().argv[0] );
+   BOOST_TEST_MESSAGE( "current binary=" << binary );
    BOOST_TEST_MESSAGE( "schema.json=" << filename );
    std::ifstream json{filename};
    if( json.is_open() ) {
