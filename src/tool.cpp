675bb078b98c168674512504d4c071010e98bfe8
Andres Sevillano <andres@chameleon.ad>
2017-03-29 18:35:17 +0100

    Added binary tool

---
+++ src/tool.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "boilerplateCodeDoc.h"
+#include "version.h"
+
+using namespace boilerplateCodeDoc;
+
+static bool processFilter(const JsonSchema& schema, const std::string& filename, JsonSchemaFilter& filter)
+{
+    bool result = filter(schema);
+
+    if( not result ) {
+
+		std::cout << filter.message << std::endl << std::endl;
+
+    } else {
+
+	try {
+		std::ofstream filterFile {filename};
+		if(filterFile) {
+
+			filterFile.write(filter.filtered.c_str(), filter.filtered.size());
+			filterFile.close(); // maybe redundant
+
+		} else {
+			std::cout << "Error opening " << filename << std::endl;
+			return false;
+		}
+
+	} catch(...) {
+		std::cout << "Unexpected exception while writing to " << filename << std::endl;
+		return 1;
+	}
+    }
+    return result;
+}
+
+int main(int argc, char** argv)
+{
+   if( argc != 4 ) {
+     std::cout << "Usage:\n\n" << argv[0] << " <Json Schema Input File Name> <HTML Output File Name> <C++ header Output File Name>\n\n";
+     return 1;
+   }
+
+   std::string schema{argv[1]};
+   std::string html{argv[2]};
+   std::string cpp{argv[3]};
+
+   JsonSchema jsonSchema{schema};
+   if(jsonSchema.error != ParseErrorCode::OK) { std::cout << jsonSchema.message << std::endl; return 1; }
+
+   JsonSchema2CPP cppFilter{};
+   bool resultCPP = processFilter(jsonSchema, cpp, cppFilter);
+
+   JsonSchema2HTML htmlFilter{};
+   bool resultHTML = processFilter(jsonSchema, html, htmlFilter);
+
+   return (resultCPP && resultHTML) ? 0 : 1;
+}
